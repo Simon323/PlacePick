9051d679796ad0f70d48faa77c89c020fc6d45ee
Szymon <szmiekus@gmail.com>
2015-05-24 22:22:57 +0200

    Prepare solution

---
+++ PlacePick.DBContextRepository/Repository/Interfaces/IEntitiesKeys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlacePick.DBContextRepository.Repository.Interfaces
+{
+    public interface IEntitiesKeys<T>
+    {
+        List<string> GetKeyNames();
+
+        List<object> GetKeys(T item);
+
+        IQueryable<T> GetByKeys(List<object> keys);
+    }
+}
