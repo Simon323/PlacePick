31e0971ab4b18b303b8ae175081287de1ac2759a
Szymon <szmiekus@gmail.com>
2015-05-24 23:29:30 +0200

    Site

---
+++ PlacePick.Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlacePick.Site.Startup))]
+namespace PlacePick.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
