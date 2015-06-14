35e6073b3dfbb181be653d1dc22593d978660638
Szymon <szmiekus@gmail.com>
2015-06-14 15:24:19 +0200

    Startowa + mapa

--- PlacePick.Site/App_Start/BundleConfig.cs
+++ PlacePick.Site/App_Start/BundleConfig.cs
@@ -26,6 +26,7 @@ namespace PlacePick.Site
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
